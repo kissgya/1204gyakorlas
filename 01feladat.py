7228d93e973ebb2955ff4be7cab3950af9eee3c6
kissgya <kissgy@kkszki.hu>
2025-12-04 16:37:08 +0100

    1. feladat kesz

--- 01feladat.py
+++ 01feladat.py
@@ -1,3 +1,10 @@
 #1. feladat – Faktoriális számítása ciklussal
 #Kérj be egy számot, és számold ki a faktoriálisát iterációval.
 
+szam = int(input("Adj meg egy egész számot: "))
+
+faktorialis = 1
+for i in range(1,szam+1):
+    faktorialis = faktorialis * i
+
+print(f"{szam}! = {faktorialis}")
