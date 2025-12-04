9f4632d1736ff37f651a9d6b6a5643439cadc178
kissgya <kissgy@kkszki.hu>
2025-12-04 17:17:46 +0100

    4. feladat kesz

--- 04feladat.py
+++ 04feladat.py
@@ -1,3 +1,24 @@
 #4. feladat – Számjegyek összege
 #Kérj be egy számot, majd számold ki a számjegyeinek összegét.
 
+szam = int(input("Adj meg egy számot: "))
+
+osszeg = 0
+temp = abs(szam)
+
+while temp > 0:
+    osszeg = osszeg + (temp % 10)
+    temp //= 10
+
+print("A számjegyek összeg:", osszeg)
+
+
+
+szam = input("Adj meg egy számot: ")
+
+osszeg = 0
+
+for i in range(len(szam)):
+    osszeg = osszeg + int(szam[i])
+
+print(f"A számjegyek összege: {osszeg}")
