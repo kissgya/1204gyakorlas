89b2791bcd14573b52794912c6aa07dd19eaa9c5
kissgya <kissgy@kkszki.hu>
2025-12-11 18:23:09 +0100

    10. feladat letrehozva

---
+++ 10feladat.py
@@ -0,0 +1,15 @@
+'''
+10. feladat – OOP: Öröklődés
+
+Készíts egy Allat alaposztályt (név, életkor).
+Készíts két leszármazottat:
+
+Kutya (plusz: fajta)
+
+Macska (plusz: szín)
+
+Mindkettőnek legyen hang() metódusa, ami kiírja a saját hangját.
+Példányosítsd őket és hívd meg a metódust.
+
+'''
+
