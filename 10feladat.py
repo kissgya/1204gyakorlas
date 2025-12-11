62fd24d8198858967991e35564a1685ae3ab468b
kissgya <kissgy@kkszki.hu>
2025-12-11 18:38:50 +0100

    10. feladat kesz

--- 10feladat.py
+++ 10feladat.py
@@ -13,3 +13,29 @@ Példányosítsd őket és hívd meg a metódust.
 
 '''
 
+class Allat:
+    def __init__(self, nev, kor):
+        self.name=nev
+        self.age=kor
+
+class Kutya(Allat):
+    def __init__(self, nev, kor, fajta):
+        super().__init__(nev, kor)
+        self.type=fajta
+
+    def hang(self):
+        print(f"{self.name} mondja: Vau-vau")
+
+class Macska(Allat):
+    def __init__(self, nev, kor, szin):
+        super().__init__(nev,kor)
+        self.color=szin
+
+    def hang(self):
+        print(f"{self.name} mondja: Miau")
+
+kutya = Kutya("Dió", 8, "Beagle")
+kutya.hang()
+
+macska = Macska("Luca", 3, "Házi")
+macska.hang()
